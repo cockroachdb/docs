27d03e3140f69886e88565ec2cbe148275d76858
Jesse Seldess <jesse@cockroachlabs.com>
2016-02-22 18:21:40 -0500

    removing unncessary js and css

--- js/customscripts.js
+++ js/customscripts.js
@@ -65,60 +65,9 @@
             else  {
                 $sidebar.css('margin-top', 40);
             }
-
-
         });
-
-
-        //hijack clicks on links, verify if anchor link and jump to div
-        //$('body').on('click', 'a', function (e) {
-        //    if (/#/.test(this.href)) {
-        //        e.preventDefault();
-
-        //        var hash = this.href.substring(this.href.indexOf('#')),
-        //            fixedElementHeight = 50;
-
-        //        $('html, body')
-        //            .stop()
-        //            .animate({
-        //                scrollTop: $(hash).offset().top - fixedElementHeight
-        //            }, 0);
-        //    }
-        //});
     });
 })(jQuery);
-
-
-// needed for nav tabs on pages. See Formatting > Nav tabs for more details.
-// script from http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
-$(function() {
-    var json, tabsState;
-    $('a[data-toggle="pill"], a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-        var href, json, parentId, tabsState;
-
-        tabsState = localStorage.getItem("tabs-state");
-        json = JSON.parse(tabsState || "{}");
-        parentId = $(e.target).parents("ul.nav.nav-pills, ul.nav.nav-tabs").attr("id");
-        href = $(e.target).attr('href');
-        json[parentId] = href;
-
-        return localStorage.setItem("tabs-state", JSON.stringify(json));
-    });
-
-    tabsState = localStorage.getItem("tabs-state");
-    json = JSON.parse(tabsState || "{}");
-
-    $.each(json, function(containerId, href) {
-        return $("#" + containerId + " a[href=" + href + "]").tab('show');
-    });
-
-    $("ul.nav.nav-pills, ul.nav.nav-tabs").each(function() {
-        var $this = $(this);
-        if (!json[$this.attr("id")]) {
-            return $this.find("a[data-toggle=tab]:first, a[data-toggle=pill]:first").tab("show");
-        }
-    });
-});
 
 
 
