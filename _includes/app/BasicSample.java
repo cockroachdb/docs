0c584ce23aedcf57dc840b0de61c8f62cb17158c
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-03-28 13:19:26 -0400

    Update the Java example.

--- _includes/app/BasicSample.java
+++ _includes/app/BasicSample.java
@@ -11,18 +11,14 @@ public class BasicSample {
         Class.forName("org.postgresql.Driver");
 
         // Connect to the "bank" DB on a local cockroach node.
-        Connection db = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:26257/bank?sslmode=disable", "root", "");
+        Connection db = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:26257/bank?sslmode=disable", "maxroach", "");
 
         try {
-            // Create the "bank" database.
-            db.createStatement().execute("CREATE DATABASE bank");
-            // Create an "accounts" table in the "bank" database.
-            db.createStatement().execute("CREATE TABLE accounts (id INT PRIMARY KEY, balance int)");
             // Insert two rows into the table.
             db.createStatement().execute("INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 230)");
 
             // Print out the balances.
-            System.out.println("Balances:");
+            System.out.println("Initial account balances:");
             ResultSet res = db.createStatement().executeQuery("SELECT id, balance FROM accounts");
             while (res.next()) {
                 System.out.printf("\taccount %s: %s\n", res.getInt("id"), res.getInt("balance"));
