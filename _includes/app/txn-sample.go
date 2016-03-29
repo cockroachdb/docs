3e58cda259718e908868b498e616a79ac1e5ff48
Andrei Matei <andrei@cockroachlabs.com>
2016-03-29 17:11:22 -0400

    go basic sample

--- _includes/app/txn-sample.go
+++ _includes/app/txn-sample.go
@@ -33,7 +33,7 @@ func transferFunds(tx *sql.Tx, from int, to int, amount int) error {
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgresql://root@localhost:26257/bank?sslmode=disable")
+	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/bank?sslmode=disable")
 	if err != nil {
 		log.Fatal("error connection to the database: ", err)
 	}
