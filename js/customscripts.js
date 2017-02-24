094b683ec913322a472a31c2e21e7fabe2ea0024
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-02-24 15:38:49 -0500

    run custom scripts on document ready, not window load
    This prevents the annoying flicker while waiting for slow images and
    third-party content.

--- js/customscripts.js
+++ js/customscripts.js
@@ -13,115 +13,113 @@ function renderTOC() {
     });
 }
 
-(function($) {
-    $(window).load(function() {
+$(function() {
 
-        var _viewport_width = $(window).width(),
-            $mobile_menu = $('nav.mobile_expanded'),
-            $sidebar = $('#mysidebar'),
-            $footer = $('section.footer'),
-            footertotop, scrolltop, difference;
+    var _viewport_width = $(window).width(),
+        $mobile_menu = $('nav.mobile_expanded'),
+        $sidebar = $('#mysidebar'),
+        $footer = $('section.footer'),
+        footertotop, scrolltop, difference;
 
-        if(_viewport_width <= 768) {
+    if(_viewport_width <= 768) {
+        $mobile_menu.css('visibility', 'visible');
+    }
+
+    $('header nav.mobile').on('click', '.hamburger', function(e){
+        e.preventDefault();
+        if($('body').hasClass('menu_open')){
+            $('body').removeClass('menu_open');
+        }
+        else {
+            $('body').addClass('menu_open');
+        }
+    });
+
+    $(window).resize(function(e){
+        _viewport_width = $(window).width();
+
+        if(_viewport_width > 768) {
+            $('body').removeClass('menu_open');
+        }
+        else {
             $mobile_menu.css('visibility', 'visible');
         }
 
-        $('header nav.mobile').on('click', '.hamburger', function(e){
-            e.preventDefault();
-            if($('body').hasClass('menu_open')){
-                $('body').removeClass('menu_open');
-            }
-            else {
-                $('body').addClass('menu_open');
-            }
-        });
-
-        $(window).resize(function(e){
-            _viewport_width = $(window).width();
-
-            if(_viewport_width > 768) {
-                $('body').removeClass('menu_open');
-            }
-            else {
-                $mobile_menu.css('visibility', 'visible');
-            }
-
-            $(window).scroll();
-        });
-
-        $(window).on('scroll', function(){
-            _viewport_width = $(window).width();
-
-            if(_viewport_width >= 992) {
-                //prevent sidebar from overlapping footer
-                footertotop = $footer.position().top;
-                scrolltop = $(document).scrollTop() + $sidebar.outerHeight() + 170;
-                difference = scrolltop-footertotop;
-
-                if (scrolltop > footertotop) {
-                    $sidebar.css('padding-top',  65-difference);
-                }
-                else  {
-                    $sidebar.css('padding-top', 65);
-                }
-            }else{
-                $sidebar.css('padding-top', 40);
-            }
-        });
-
         $(window).scroll();
-
-        // Section makes shell terminal prompt markers ($) totally unselectable in syntax-highlighted code samples
-        terminalMarkers = document.getElementsByClassName("gp");  // Rogue syntax highlighter styles all terminal markers with class gp
-
-        for(var i = 0; i < terminalMarkers.length; i++){
-            terminalMarkers[i].innerText="";    // Remove the existing on-page terminal marker
-            terminalMarkers[i].className += " noselect shellterminal"; // Add shellterminal class, which then displays the terminal marker as a ::before element
-        }
-
-        // Section makes SQL terminal prompt markers (>) totally unselectable in syntax-highlighted code samples
-        sqlMarkers = document.getElementsByClassName("o");
-        for(var i = 0; i < sqlMarkers.length; i++){
-            if(sqlMarkers[i].innerText===">" && (!sqlMarkers[i].previousSibling || sqlMarkers[i].previousSibling.textContent==="\n"|| sqlMarkers[i].previousSibling.textContent==="\n\n")){
-                sqlMarkers[i].innerText="";    // Remove the existing on-page SQL marker
-                sqlMarkers[i].nextSibling.textContent="";
-                sqlMarkers[i].className += " noselect sqlterminal"; // Add sqlterminal class, which then displays the terminal marker as a ::before element
-            }
-        }
-
-        // Render the TOC on DOM ready by default.
-        renderTOC();
-
-        // Activate a new filter scope by setting the `current` class on only
-        // elements with the desired scope and re-rendering the TOC to reflect any
-        // changes in visibility.
-        function setFilterScope(scope) {
-            ['.filter-button', '.filter-content'].forEach(function (selector) {
-                $(selector + '[data-scope].current').removeClass('current');
-                $(selector + '[data-scope="' + scope + '"]').addClass('current');
-            })
-            renderTOC();
-        }
-
-        // Handle clicks on filter buttons by activating the scope named by that
-        // button and updating the URL hash.
-        $('.filter-button').on('click', function() {
-            var scope = $(this).data('scope');
-            var url = window.location.pathname + window.location.search +
-                ($(this).is(':first-child') ? '' : '#' + scope);
-            setFilterScope(scope);
-            history && history.pushState(null, null, url);
-        });
-
-        // Handle URL hash changes by attempting to activate the scope named in the
-        // hash. If no such scope exists, instead activate the scope named by the
-        // button that appears first in DOM order.
-        $(window).on('hashchange', function () {
-            var scope = window.location.hash.substring(1);
-            if ($('.filter-button[data-scope="' + scope + '"]').length == 0) {
-                scope = $('.filter-button[data-scope]').first().data('scope');
-            }
-            setFilterScope(scope);
-        }).trigger('hashchange');
     });
-})(jQuery);
+
+    $(window).on('scroll', function(){
+        _viewport_width = $(window).width();
+
+        if(_viewport_width >= 992) {
+            //prevent sidebar from overlapping footer
+            footertotop = $footer.position().top;
+            scrolltop = $(document).scrollTop() + $sidebar.outerHeight() + 170;
+            difference = scrolltop-footertotop;
+
+            if (scrolltop > footertotop) {
+                $sidebar.css('padding-top',  65-difference);
+            }
+            else  {
+                $sidebar.css('padding-top', 65);
+            }
+        }else{
+            $sidebar.css('padding-top', 40);
+        }
+    });
+
+    $(window).scroll();
+
+    // Section makes shell terminal prompt markers ($) totally unselectable in syntax-highlighted code samples
+    terminalMarkers = document.getElementsByClassName("gp");  // Rogue syntax highlighter styles all terminal markers with class gp
+
+    for(var i = 0; i < terminalMarkers.length; i++){
+        terminalMarkers[i].innerText="";    // Remove the existing on-page terminal marker
+        terminalMarkers[i].className += " noselect shellterminal"; // Add shellterminal class, which then displays the terminal marker as a ::before element
+    }
+
+    // Section makes SQL terminal prompt markers (>) totally unselectable in syntax-highlighted code samples
+    sqlMarkers = document.getElementsByClassName("o");
+    for(var i = 0; i < sqlMarkers.length; i++){
+        if(sqlMarkers[i].innerText===">" && (!sqlMarkers[i].previousSibling || sqlMarkers[i].previousSibling.textContent==="\n"|| sqlMarkers[i].previousSibling.textContent==="\n\n")){
+            sqlMarkers[i].innerText="";    // Remove the existing on-page SQL marker
+            sqlMarkers[i].nextSibling.textContent="";
+            sqlMarkers[i].className += " noselect sqlterminal"; // Add sqlterminal class, which then displays the terminal marker as a ::before element
+        }
+    }
+
+    // Render the TOC on DOM ready by default.
+    renderTOC();
+
+    // Activate a new filter scope by setting the `current` class on only
+    // elements with the desired scope and re-rendering the TOC to reflect any
+    // changes in visibility.
+    function setFilterScope(scope) {
+        ['.filter-button', '.filter-content'].forEach(function (selector) {
+            $(selector + '[data-scope].current').removeClass('current');
+            $(selector + '[data-scope="' + scope + '"]').addClass('current');
+        })
+        renderTOC();
+    }
+
+    // Handle clicks on filter buttons by activating the scope named by that
+    // button and updating the URL hash.
+    $('.filter-button').on('click', function() {
+        var scope = $(this).data('scope');
+        var url = window.location.pathname + window.location.search +
+            ($(this).is(':first-child') ? '' : '#' + scope);
+        setFilterScope(scope);
+        history && history.pushState(null, null, url);
+    });
+
+    // Handle URL hash changes by attempting to activate the scope named in the
+    // hash. If no such scope exists, instead activate the scope named by the
+    // button that appears first in DOM order.
+    $(window).on('hashchange', function () {
+        var scope = window.location.hash.substring(1);
+        if ($('.filter-button[data-scope="' + scope + '"]').length == 0) {
+            scope = $('.filter-button[data-scope]').first().data('scope');
+        }
+        setFilterScope(scope);
+    }).trigger('hashchange');
+});
