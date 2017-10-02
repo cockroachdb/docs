a7e18f99b5c498ba0c57542304954ff076b88477
Jesse Seldess <jesse@cockroachlabs.com>
2017-10-02 12:20:16 -0400

    Fix 1.1 SHOW CONSTRAINTS diagram

--- generate/main.go
+++ generate/main.go
@@ -554,7 +554,12 @@ func main() {
 					replace: map[string]string{"var_name": "table_name"},
 					unlink: []string{"table_name"},
 				},
-				{name: "show_constraints", stmt: "show_stmt", match: []*regexp.Regexp{regexp.MustCompile("'SHOW' 'CONSTRAINTS'")}, replace: map[string]string{"var_name": "table_name"}, unlink: []string{"table_name"}},
+				{
+					name: "show_constraints",
+					stmt: "show_constraints_stmt",
+					replace: map[string]string{"var_name": "table_name"},
+					unlink: []string{"table_name"},
+				},
 				{name: "show_create_table", stmt: "show_stmt", match: []*regexp.Regexp{regexp.MustCompile("'SHOW' 'CREATE' 'TABLE'")}, replace: map[string]string{"var_name": "table_name"}, unlink: []string{"table_name"}},
 				{name: "show_create_view", stmt: "show_stmt", match: []*regexp.Regexp{regexp.MustCompile("'SHOW' 'CREATE' 'VIEW'")}, replace: map[string]string{"var_name": "view_name"}, unlink: []string{"view_name"}},
 				{name: "show_databases", stmt: "show_stmt", match: []*regexp.Regexp{regexp.MustCompile("'SHOW' 'DATABASES'")}},
