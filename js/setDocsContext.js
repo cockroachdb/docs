114ee997eb400fb86949549189a46436cd798e50
Joseph Lowinske <joe@cockroachlabs.com>
2018-03-20 11:39:05 -0400

    Add 'search by Algolia' image; add vendor file for autocomplete feature

--- js/setDocsContext.js
+++ js/setDocsContext.js
@@ -1,15 +1,11 @@
 $(window).load(function(e) {
   var docsContext = location.href.split('/')[4];
 
-  if (docsContext.indexOf("search") > -1) return;
+  if (docsContext.indexOf("search") > -1) {
+    return;
+  }
 
   setSiteCookie('currentVersion', docsContext);
 
   return;
-
-  function setSiteCookie(key, value) {
-    var expires = new Date();
-    expires.setTime(expires.getTime() + (7 * 24 * 60 * 60 * 1000));
-    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + ';path=/docs/';
-  }
 });
