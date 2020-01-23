558a6b6b4d7d1e8b145a7ada916ada6e05895b5a
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2020-01-23 11:02:10 -0500

    Django docs rewritten. Some code/config files updated.

--- _includes/v20.1/app/django-basic-sample/urls.py
+++ _includes/v20.1/app/django-basic-sample/urls.py
@@ -1,4 +1,4 @@
-"""cockroach_example URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- _includes/v20.1/app/django-basic-sample/wsgi.py
+++ _includes/v20.1/app/django-basic-sample/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cockroach_example project.
+WSGI config for myproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cockroach_example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
