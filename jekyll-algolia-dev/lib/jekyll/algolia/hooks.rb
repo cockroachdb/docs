36048dda1da3941c906bb161b1cec32f0b1bd137
Peter Panagiotakos <peterp@cockroachlabs.com>
2020-03-03 13:10:16 -0500

    inline our urls

--- jekyll-algolia-dev/lib/jekyll/algolia/hooks.rb
+++ jekyll-algolia-dev/lib/jekyll/algolia/hooks.rb
@@ -97,9 +97,10 @@ module Jekyll
 
         if context.config['cockroachcloud']
           record[:doc_type] = 'cockroachcloud'
-          record[:url] = "/cockroachcloud#{record[:url]}"
-          record[:canonical] = "/cockroachcloud#{record[:url]}"
+          record[:url] = "https://www.cockroachlabs.com/docs/cockroachcloud#{record[:url]}"
+          record[:canonical] = "https://www.cockroachlabs.com/docs/cockroachcloud#{record[:url]}"
         else
+          record[:url] = "https://www.cockroachlabs.com/docs/#{record[:url]}"
           record[:doc_type]  = 'cockroachdb'
         end
         record
