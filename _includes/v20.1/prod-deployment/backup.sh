49787b035a93ceef7fd6d5316d1f8c90b21e7359
Lauren <lauren@cockroachlabs.com>
2020-03-11 15:13:44 -0400

    Full-cluster BACKUP / RESTORE + incremental backups
    Closes #6005, #6440, #6532
    
    Edits based on Paul's feedback
    
    Update backup.sh
    
    Edits based on Eric's feedback

--- _includes/v20.1/prod-deployment/backup.sh
+++ _includes/v20.1/prod-deployment/backup.sh
@@ -6,8 +6,7 @@ set -euo pipefail
 # day of the week and incremental backups when run on other days, and tracks
 # recently created backups in a file to pass as the base for incremental backups.
 
-full_day="<day_of_the_week>"                        # Must match (including case) the output of `LC_ALL=C date +%A`.
-what="DATABASE <database_name>"                   # The name of the database you want to backup.
+what=""                                           # Leave empty for full cluster backup, or add "DATABASE database_name" to backup a database.
 base="<storage_URL>/backups"                      # The URL where you want to store the backup.
 extra="<storage_parameters>"                      # Any additional parameters that need to be appended to the BACKUP URI e.g. AWS key params.
 recent=recent_backups.txt                         # File in which recent backups are tracked.
@@ -16,21 +15,7 @@ backup_parameters=<additional backup parameters>  # e.g. "WITH revision_history"
 # Customize the `cockroach sql` command with `--host`, `--certs-dir` or `--insecure`, `--port`, and additional flags as needed to connect to the SQL client.
 runsql() { cockroach sql --insecure -e "$1"; }
 
-destination="${base}/$(date +"%Y%m%d-%H%M")${extra}"
+destination="${base}/$(date +"%Y-%V")${extra}" # %V is the week number of the year, with Monday as the first day of the week.
 
-prev=
-while read -r line; do
-    [[ "$prev" ]] && prev+=", "
-    prev+="'$line'"
-done < "$recent"
-
-if [[ "$(LC_ALL=C date +%A)" = "$full_day" || ! "$prev" ]]; then
-    runsql "BACKUP $what TO '$destination' AS OF SYSTEM TIME '-1m' $backup_parameters"
-    echo "$destination" > "$recent"
-else
-    destination="${base}/$(date +"%Y%m%d-%H%M")-inc${extra}"
-    runsql "BACKUP $what TO '$destination' AS OF SYSTEM TIME '-1m' INCREMENTAL FROM $prev $backup_parameters"
-    echo "$destination" >> "$recent"
-fi
-
+runsql "BACKUP $what TO '$destination' AS OF SYSTEM TIME '-1m' $backup_parameters"
 echo "backed up to ${destination}"
