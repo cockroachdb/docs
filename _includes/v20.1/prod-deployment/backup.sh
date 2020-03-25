62d0a87f548b33c24bb2ca58d7d80ab666313331
Lauren <lauren@cockroachlabs.com>
2020-03-24 20:21:34 -0400

    Update backup.sh

--- _includes/v20.1/prod-deployment/backup.sh
+++ _includes/v20.1/prod-deployment/backup.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 # day of the week and incremental backups when run on other days, and tracks
 # recently created backups in a file to pass as the base for incremental backups.
 
-full_day="<day_of_the_week>"                      # Must match (including case) the output of `LC_ALL=C date +%A`.
 what=""                                           # Leave empty for full cluster backup, or add "DATABASE database_name" to backup a database.
 base="<storage_URL>/backups"                      # The URL where you want to store the backup.
 extra="<storage_parameters>"                      # Any additional parameters that need to be appended to the BACKUP URI e.g. AWS key params.
@@ -16,11 +15,7 @@ backup_parameters=<additional backup parameters>  # e.g. "WITH revision_history"
 # Customize the `cockroach sql` command with `--host`, `--certs-dir` or `--insecure`, `--port`, and additional flags as needed to connect to the SQL client.
 runsql() { cockroach sql --insecure -e "$1"; }
 
-destination="${base}/$(date +"%Y%m%d-%H%M")${extra}"
-
-current=$(tail -1 "$recent")
+destination="${base}/$(date +"%Y-%V")${extra}" # %V is the week number of the year, with Monday as the first day of the week.
 
 runsql "BACKUP $what TO '$destination' AS OF SYSTEM TIME '-1m' $backup_parameters"
-echo "$destination" > "$recent"
-
 echo "backed up to ${destination}"
