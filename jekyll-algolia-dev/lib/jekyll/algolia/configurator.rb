dbc76a1eec7046ecfd72293b6a437ae407223894
yingzhuchin <63265176+yingzhuchin@users.noreply.github.com>
2020-05-18 16:01:38 -0400

    Revert "Add list items to search indexing (Fixes 7351)"

--- jekyll-algolia-dev/lib/jekyll/algolia/configurator.rb
+++ jekyll-algolia-dev/lib/jekyll/algolia/configurator.rb
@@ -12,7 +12,7 @@ module Jekyll
       ALGOLIA_DEFAULTS = {
         'extensions_to_index' => nil,
         'files_to_exclude' => nil,
-        'nodes_to_index' => 'p,td,li',
+        'nodes_to_index' => 'p,td',
         'indexing_batch_size' => 1000,
         'max_record_size' => 20_000,
         'settings' => {
