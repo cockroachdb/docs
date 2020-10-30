fceef973ca1710fddde6eed143392005cec890e1
Jesse Seldess <j_seldess@hotmail.com>
2020-10-30 15:27:19 -0400

    Update json katacoda tutorial
    Same as python tutorial, have user initiate the steps
    of downloading and starting cockroach.

--- tutorials/katacoda/json-support/foreground.sh
+++
@@ -1,3 +0,0 @@
-wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.0-rc.2.linux-amd64.tgz | tar  xvz
-cp -i cockroach-v20.2.0-rc.2.linux-amd64/cockroach /usr/local/bin/
-cockroach start-single-node --insecure --listen-addr=localhost:26257 --http-addr=localhost:8080
