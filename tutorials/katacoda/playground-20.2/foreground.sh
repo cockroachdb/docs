7ee5e33554a3ebff26ba742198323b8676b98860
Jesse Seldess <j_seldess@hotmail.com>
2021-01-22 21:20:58 -0500

    Secure 20.2 playground cluster

--- tutorials/katacoda/playground-20.2/foreground.sh
+++ tutorials/katacoda/playground-20.2/foreground.sh
@@ -3,6 +3,10 @@ cp -i cockroach-v20.2.4.linux-amd64/cockroach /usr/local/bin/
 mkdir -p /usr/local/lib/cockroach
 cp -i cockroach-v20.2.4.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
 cp -i cockroach-v20.2.4.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
-cockroach start-single-node --insecure --background
-cockroach workload init movr
-cockroach sql --database=movr --insecure
+mkdir certs my-safe-directory
+cockroach cert create-ca --certs-dir=certs --ca-key=my-safe-directory/ca.key
+cockroach cert create-node localhost $(hostname) --certs-dir=certs --ca-key=my-safe-directory/ca.key
+cockroach cert create-client root --certs-dir=certs --ca-key=my-safe-directory/ca.key
+cockroach start-single-node --certs-dir=certs --listen-addr=localhost:26257 --http-addr=localhost:8080 --background
+cockroach workload init movr 'postgres://root@localhost:26257?sslmode=verify-full&sslrootcert=certs/ca.crt&sslcert=certs/client.root.crt&sslkey=certs/client.root.key
+cockroach sql --database=movr --certs-dir=certs
