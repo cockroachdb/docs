90e17099d3701afb799c4d449b37584ef077dfd0
Jesse Seldess <j_seldess@hotmail.com>
2021-01-22 19:55:10 -0500

    Add playground running 20.2 with cockroach startt-single-node

---
+++ tutorials/katacoda/playground-20.2/background.sh
@@ -0,0 +1,6 @@
+wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.4.linux-amd64.tgz | tar  xvz
+cp -i cockroach-v20.2.4.linux-amd64/cockroach /usr/local/bin/
+mkdir -p /usr/local/lib/cockroach
+cp -i cockroach-v20.2.4.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
+cp -i cockroach-v20.2.4.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
+cockroach start-single-node --insecure
