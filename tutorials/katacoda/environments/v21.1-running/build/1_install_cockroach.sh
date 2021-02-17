5982e7284358eafeee92f689a98827ece14c621b
Jesse Seldess <j_seldess@hotmail.com>
2021-02-17 09:52:08 -0500

    Upgrade Katacodas to 20.2.5
    Also remove the unused /environments directory.

--- tutorials/katacoda/environments/v21.1-running/build/1_install_cockroach.sh
+++
@@ -1,5 +0,0 @@
-wget -qO- https://binaries.cockroachdb.com/cockroach-v21.1.0-alpha.1.linux-amd64.tgz | tar  xvz
-cp -i cockroach-v21.1.0-alpha.1.linux-amd64/cockroach /usr/local/bin/
-mkdir -p /usr/local/lib/cockroach
-cp -i cockroach-v21.1.0-alpha.1.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
-cp -i cockroach-v21.1.0-alpha.1.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
--- tutorials/katacoda/environments/v21.1-running/build/2_configure_environment.sh
+++
@@ -1,5 +0,0 @@
-cat << EOF > /opt/configure-environment.sh
-#!/bin/bash
-cockroach demo
-EOF
-chmod +x /opt/configure-environment.sh
