571601175c082d0beb305f5ab4c355ee0e5c5381
Jesse Seldess <j_seldess@hotmail.com>
2021-03-16 10:31:27 -0400

    Upgrade katacoda tutorials to v20.2.6

--- tutorials/katacoda/build-python-app/foreground.sh
+++ tutorials/katacoda/build-python-app/foreground.sh
@@ -1,7 +1,7 @@
 pip install psycopg2-binary
-wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.5.linux-amd64.tgz | tar  xvz
-cp -i cockroach-v20.2.5.linux-amd64/cockroach /usr/local/bin/
+wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.6.linux-amd64.tgz | tar  xvz
+cp -i cockroach-v20.2.6.linux-amd64/cockroach /usr/local/bin/
 mkdir -p /usr/local/lib/cockroach
-cp -i cockroach-v20.2.5.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
-cp -i cockroach-v20.2.5.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
+cp -i cockroach-v20.2.6.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
+cp -i cockroach-v20.2.6.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
 exec cockroach demo --empty
