05b9ad0bec3b1d2114a1a994b50cc59f79df1823
Jesse Seldess <jesse@cockroachlabs.com>
2021-03-19 13:18:38 -0400

    Remove Katacoda source files from docs repo
    Follow-up to https://github.com/cockroachdb/katacoda/pull/1,
    which moved the files to a new repo specific for Katacoda
    content.

--- tutorials/katacoda/json-support/foreground.sh
+++
@@ -1,3 +0,0 @@
-wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.6.linux-amd64.tgz | tar  xvz
-cp -i cockroach-v20.2.6.linux-amd64/cockroach /usr/local/bin/
-cockroach start-single-node --insecure
