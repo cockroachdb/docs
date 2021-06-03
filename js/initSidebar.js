18dfcf85ffe2e1841c41e90027cbe74cc980ca5b
Peter Panagiotakos <peterp@cockroachlabs.com>
2021-06-03 09:14:26 -0400

    comment out navs for speed test

--- js/initSidebar.js
+++ js/initSidebar.js
@@ -1,47 +1,47 @@
-$(document).ready(function() {
-  var $sidebar = $('#sidebar');
-  var $sidebars = $('.js-sidebar');
+// $(document).ready(function() {
+//   var $sidebar = $('#sidebar');
+//   var $sidebars = $('.js-sidebar');
 
-  var navgocoOptions = {
-    caretHtml: '',
-    accordion: true,
-    openClass: 'active',
-    save: false,
-    cookie: {
-      name: 'navgoco',
-      expires: false,
-      path: '/'
-    },
-    slide: {
-      duration: 150,
-      easing: 'swing'
-    }
-  };
+//   var navgocoOptions = {
+//     caretHtml: '',
+//     accordion: true,
+//     openClass: 'active',
+//     save: false,
+//     cookie: {
+//       name: 'navgoco',
+//       expires: false,
+//       path: '/'
+//     },
+//     slide: {
+//       duration: 150,
+//       easing: 'swing'
+//     }
+//   };
 
-  // Initialize navgoco with config options
-  $sidebar.navgoco($.extend(navgocoOptions, {
-    onToggleAfter: function() {
-      if ($(this.el).attr('id') == 'sidebar' &&
-          $('#version-switcher').hasClass('open')) {
-        closeVersionSwitcher();
-      }
-    }
-  }));
+//   // Initialize navgoco with config options
+//   $sidebar.navgoco($.extend(navgocoOptions, {
+//     onToggleAfter: function() {
+//       if ($(this.el).attr('id') == 'sidebar' &&
+//           $('#version-switcher').hasClass('open')) {
+//         closeVersionSwitcher();
+//       }
+//     }
+//   }));
 
-  $sidebar.show();
+//   $sidebar.show();
 
-  // Loop over the left sidebar and top nav mobile version
-  $sidebars.each((k, sidebar)=>{
-    // console.log(sidebar);
-    // sidebar.navgoco($.extend(navgocoOptions, {
-    //   onToggleAfter: function() {
-    //     if ($(this.el).attr('id') == 'sidebar' &&
-    //         $('#version-switcher').hasClass('open')) {
-    //       closeVersionSwitcher();
-    //     }
-    //   }
-    // }));
-    // sidebar.show();
-  });
+//   // Loop over the left sidebar and top nav mobile version
+//   $sidebars.each((k, sidebar)=>{
+//     // console.log(sidebar);
+//     // sidebar.navgoco($.extend(navgocoOptions, {
+//     //   onToggleAfter: function() {
+//     //     if ($(this.el).attr('id') == 'sidebar' &&
+//     //         $('#version-switcher').hasClass('open')) {
+//     //       closeVersionSwitcher();
+//     //     }
+//     //   }
+//     // }));
+//     // sidebar.show();
+//   });
 
-});
+// });
