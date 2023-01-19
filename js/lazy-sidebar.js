474d7a4b5e5a72a2fedcd02a37ce22afb913942a
Angel Carvajal <angel_adcm@hotmail.com>
2023-01-19 12:25:15 -0600

    feat: lazy load sidebar logic on demand

---
+++ js/lazy-sidebar.js
@@ -0,0 +1,49 @@
+$(function() {
+  var pageVersion = (function () {
+    var pathComponents = location.pathname
+      .replace(sidebar.baseUrl, '')
+      .replace(/^\//, '')
+      .split('/');
+    // The version is the first directory component in the URL,
+    // if it exists.
+    if (pathComponents.length > 1 && sidebar.isVersionDirectory(pathComponents[0])) {
+      return pathComponents[0];
+    }
+    // Non-versioned pages link to stable docs.
+    return "stable";
+  })();
+  var sidebarData = window.sidebar;
+  var mainTiers = $('.tier-1');
+  function generateSideBar(sidebarItems, tierElements, tierId) {
+    sidebarItems.forEach(function(mainItem, index) {
+      if (!mainItem.items || mainItem.items.length === 0) {
+        return;
+      }
+      $(tierElements[index]).find('a').on('click', function(currentElement) {
+        if (mainItem.alreadyIterated) {
+          return;
+        }
+        var nextTier = tierId + 1;
+        var htmlToAppend = '<ul>';
+        mainItem.items.forEach(function(childItem) {
+          htmlToAppend += `<li class="tier-${nextTier}">`
+          var currentAnchorUrl = '#';
+          var subsectionArrow = '<div class="nav-expand"></div>';
+          if (childItem.urls) {
+            currentAnchorUrl = childItem.urls[0].replace('${VERSION}', pageVersion);
+            subsectionArrow = '';
+          }
+          htmlToAppend += `
+            <a href="${currentAnchorUrl}">${childItem.title} ${subsectionArrow}</a>
+          `;
+          htmlToAppend += `</li>`;
+        });
+        htmlToAppend += '</ul>';
+        $(currentElement.currentTarget).parent().append(htmlToAppend);
+        var currentTiers = $(currentElement.currentTarget).parent().find('.tier-' + nextTier);
+        generateSideBar(mainItem.items, currentTiers, nextTier);
+      });
+    });
+  }
+  generateSideBar(sidebarData.items, mainTiers, 1);
+});
