df2b3c09b3e68ad4b02d9295cd181e9535a5a7b2
mikeCRL <mike@cockroachlabs.com>
2023-11-06 11:25:31 -0500

    Additional verbosity to build script output

--- src/current/netlify/build-dev.sh
+++ src/current/netlify/build-dev.sh
@@ -2,16 +2,18 @@
 
 # Populate the site_url to be used by jekyll for generating sidebar and search links
 site_url="${DEPLOY_PRIME_URL}" # this is set by Netlify for the site
+echo "site_url defaulted to Netlify DEPLOY_PRIME_URL: ${site_url}"
 JEKYLL_ENV="preview"
 if [[ "$CONTEXT" = "production" ]]; then
 	site_url="https://dev.cockroachlabs.com" # temporary placeholder to differentiate from production URL
+    echo "site_url updated to: ${site_url}"
 	JEKYLL_ENV="production" # even in the dev repo/site, we'll maintain production vs preview builds; these are Netlify designations
 fi;
 echo "url: ${site_url}" > _config_url.yml
 
 # Builds the site
 function build {
-	bundle exec jekyll build --trace --config _config_base-dev.yml,$1 #adds parameters from build call, _config_cockroachdb.yml,_config_url.yml
+	bundle exec jekyll build --trace --verbose --config _config_base-dev.yml,$1 #adds parameters from build call, _config_cockroachdb.yml,_config_url.yml
 	if [[ $? != 0 ]]; then
 	  exit 1
 	fi;
