b912b5d5417250315b79771d149ad3679526e84e
ebembi-crdb <ebembi.bembi@cockroachlabs.com>
2024-07-24 21:49:06 +0530

    testing

--- src/current/netlify/edge-functions/blockBytedance.js
+++ src/current/netlify/edge-functions/blockBytedance.js
@@ -1,18 +1,22 @@
 export default async (request) => {
-    const bytedanceUserAgents = [
-      'Bytespider; spider-feedback@bytedance.com'
-    ];
-  
-    const userAgent = request.headers.get('user-agent') || '';
-  
-    if (bytedanceUserAgents.some(ua => userAgent.includes(ua))) {
-      return new Response('Access Denied', {
-        status: 403,
-        headers: {
-          'Content-Type': 'text/plain'
-        }
-      });
-    }
-  
-    return fetch(request);
-  };
+  const bytedanceUserAgents = [
+    'Bytespider; spider-feedback@bytedance.com'
+  ];
+
+  const userAgent = request.headers.get('user-agent') || '';
+
+  if (bytedanceUserAgents.some(ua => userAgent.includes(ua))) {
+    return new Response('Access Denied', {
+      status: 403,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    });
+  }
+
+  return fetch(request);
+};
+
+export const config = {
+path: "/*",
+};
